277308ee8fd33edadc05010fa59d4ee0375d73cc
Matheus-Freitas0 <moncaomatheus@gmail.com>
2025-06-21 19:30:32 -0300

    WIP - create controller authentication and private

---
+++ src/main/java/com/poc/springSecurity/controller/AuthenticationController.java
@@ -0,0 +1,19 @@
+package com.poc.springSecurity.controller;
+
+import com.poc.springSecurity.service.AuthenticationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthenticationController {
+
+    @Autowired
+    private AuthenticationService authenticationService;
+
+    @PostMapping("authenticate")
+    public String authenticate() {
+        return authenticationService.authenticate();
+    }
+
+}
---
+++ src/main/java/com/poc/springSecurity/service/AuthenticationService.java
@@ -0,0 +1,10 @@
+package com.poc.springSecurity.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationService {
+    public String authenticate() {
+        return "token";
+    }
+}
