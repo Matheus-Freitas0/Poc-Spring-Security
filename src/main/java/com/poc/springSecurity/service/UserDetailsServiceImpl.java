1003f3e3ff96a54cdad379c2bba566d9bf1a1b41
Matheus-Freitas0 <moncaomatheus@gmail.com>
2025-06-21 21:10:59 -0300

    WIP - create UserDetailsServiceImpl

---
+++ src/main/java/com/poc/springSecurity/service/UserDetailsServiceImpl.java
@@ -0,0 +1,21 @@
+package com.poc.springSecurity.service;
+
+import com.poc.springSecurity.domain.UserAuthenticated;
+import com.poc.springSecurity.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username)
+                .map(UserAuthenticated -> new UserAuthenticated())
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+}
